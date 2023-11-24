785a48bf253863ed73da671b176dad348e7f91cf
乐之 <zlz@zlzdeMacBook-Pro.local>
2023-11-25 06:55:24 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+import pygame
+import RPi.GPIO as GPIO
+import ddeeff
+ddeeff.huataishu(300,1)
+ddeeff.huataiheng(850,1)
+ddeeff.huataishu(250,0)
+ddeeff.xi(4)
+ddeeff.huataishu(500,1)
+ddeeff.xi(2)
+ddeeff.huataishu(500,1)
+ddeeff.xi(2)
+ddeeff.huataiheng(400,1)
+ddeeff.xi(2)
+ddeeff.huataiheng(300,1)
+ddeeff.huataishu(40,0)
+ddeeff.fang()
+ddeeff.huataishu(40,1)
+ddeeff.huataiheng(1600,0)
+ddeeff.huataishu(1200,0)
